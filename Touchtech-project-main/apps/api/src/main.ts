183fc3698a1c4254213b52cf43b7df7379b00b25
tyl2r <138535098+tyl2r@users.noreply.github.com>
2026-06-04 09:34:45 +0300

    Update main.ts

--- Touchtech-project-main/apps/api/src/main.ts
+++ Touchtech-project-main/apps/api/src/main.ts
@@ -60,6 +60,8 @@ async function bootstrap(): Promise<void> {
 
   app.enableShutdownHooks();
 
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
   await app.listen({ port, host: '0.0.0.0' });
 }
 
